0fc46c9c72c2f0a8c71a385f0dc09f4fde3ce7df
mx3L <mx3ldev@gmail.com>
2015-11-11 18:18:24 +0100

    [basedonvu] define ADDON_HELPER_ARCH for dvbbox to mips

--- addons/library.xbmc.addon/libXBMC_addon.h
+++ addons/library.xbmc.addon/libXBMC_addon.h
@@ -55,6 +55,8 @@ typedef intptr_t      ssize_t;
 #define ADDON_HELPER_ARCH       "powerpc64-linux"
 #elif defined(__ARMEL__)
 #define ADDON_HELPER_ARCH       "arm"
+#elif defined(TARGET_DVBBOX) // oskwon
+#define ADDON_HELPER_ARCH       "mipsel-oe-linux"
 #elif defined(__mips__)
 #define ADDON_HELPER_ARCH       "mips"
 #else
